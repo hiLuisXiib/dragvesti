4cb0d91fda4444e0b869928131d75a3dd0a855df
Xiiber <helloxiiber@gmail.com>
2021-05-28 12:13:59 -0500

    100-92-100-91
    Se cambio accesibilidad con la lista de magenes  y SEo con descripción.

---
+++ image-list.js
@@ -0,0 +1,23 @@
+var Path = 'fotos/';
+
+var Gallery = {
+    'images': [
+
+        {
+            'name': 'Coronación en el Teatro Hipódromo Condesa',
+            'alt': 'Cynthia Lee Fontaine, Iris XC queen y Luis Maciel en Hipódromo Condesa',
+            'url': 'fotos/cynthia_lee_fontaine_iris_xc_queen_mexico.webp',
+            'credit': '<a href="https://dragvesti.xiiber.com/fotos/cynthia_lee_fontaine_iris_xc_queen_mexico.webp">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
+        },
+
+        {
+            'name': 'Dragvesti Chihuahua, Guerra de lipsync',
+            'alt': 'Host Iris XC Queen en Lulu´s',
+            'url': 'fotos/dragvesti_chihuahua_guerra_de_lipsync.webp',
+            'credit': '<a href="https://dragvesti.xiiber.com/fotos/dragvesti_chihuahua_guerra_de_lipsync.webp">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
+        },
+
+
+
+    ]
+};
